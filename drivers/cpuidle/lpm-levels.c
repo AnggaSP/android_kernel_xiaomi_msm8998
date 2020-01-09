08fa2ecba2fb0224c9a8535f5e2f1c7306c0be93
Raghavendra Kakarla <rkakarla@codeaurora.org>
2020-01-09 19:09:13 +0530

    drivers: lpm-levels: Use cpu_do_idle for shallower LPM entry
    dsb should be invoked before wfi instruction without which
    core can get stuck or do not respond to IPIs. Use cpu_do_idle
    that invokes both dsb and wfi instructions for shallower mode
    entry.
    
    Change-Id: I2e9d2317048781590a626a0b3d93f23bbf9eb496
    Signed-off-by: Raghavendra Kakarla <rkakarla@codeaurora.org>
    Signed-off-by: Angga Satya Putra <anggasp@hotmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  * Copyright (C) 2006-2007 Adam Belay <abelay@novell.com>
  * Copyright (C) 2009 Intel Corporation
  *
@@ -1480,7 +1480,7 @@ bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 	 */
 	if (!idx) {
 		stop_critical_timings();
-		wfi();
+		cpu_do_idle();
 		start_critical_timings();
 		return 1;
 	} else {
