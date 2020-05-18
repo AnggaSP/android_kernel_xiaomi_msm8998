8e1366e8d7af5d2f2504fb6c89d74edec4584956
Angga Satya Putra <anggasp@hotmail.com>
2020-05-18 22:59:29 +0700

    msm-4.4: Fix strlcpy size args
    The destination length is not defined as it was just allocated so
    rather than using strlen, get the size instead.
    
    This fix the string being empty :/
    
    Fixes: 1be50c76a49b3afa890dd101b13e8c57b7b793a8 ("msm-4.4: Fix strlcpy-strlcat-size Clang warnings")
    Signed-off-by: Angga Satya Putra <anggasp@hotmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -6352,7 +6352,7 @@ static int fetch_cpu_mitigaiton_info(struct msm_thermal_data *data,
 			goto fetch_mitig_exit;
 		}
 		strlcpy((char *) cpus[_cpu].sensor_type, sensor_name,
-			strlen((char *) cpus[_cpu].sensor_type) + 1);
+			sizeof((char *) cpus[_cpu].sensor_type));
 		create_alias_name(_cpu, limits, pdev);
 	}
 
